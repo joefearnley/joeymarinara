eb68db5c08858aa6e90abd642d54ade23b5ce4d4
Joe Fearnley <joe.fearnley@gmail.com>
2021-05-11 22:41:54 -0400

    Adding html to markdown conversion

--- conversion/index.js
+++ conversion/index.js
@@ -1,20 +1,44 @@
-
+const fs = require('fs');
 const cheerio = require('cheerio');
 const TurndownService = require('turndown')
 
-const $ = cheerio.load('');
+const postsDir = '../posts/html/';
+const outputDir = '../posts/markdown/';
+let $ = null;
 
+fs.readdir(postsDir, (err, filesnames) => {
+    if (err) {
+        console.log('Error reading file directory: ' + filesnames);
+        console.log(err);
+        return;
+    }
 
-let turndownService = new TurndownService()
-let markdown = turndownService.turndown('');
+    filesnames.forEach(filename => {
+        fs.readFile(postsDir + filename, 'utf-8', function(err, content) {
+            if (err) {
+                console.log('Error reading file:' + filename);
+                console.log(err);
+                return;
+            }
 
+            // manipulate the dom (if need be)
+            $ = cheerio.load(content);
+            let html = $('body').html();
 
-// read in files
+            // remove blank lines to clean things up a bit.
+            // and remove extra space on each line
+            html = html.replace(/(^[ \t]*\n)/gm, "")
+                .split("\n")
+                .map(line => line.trim())
+                .join('');
 
-// for each file :
+            // convert html to markdown
+            turndownService = new TurndownService();
+            let markdown = turndownService.turndown(html);
 
-// manipulate the dom (if need be)
-
-// convert html to markdown
-
-// write file with .md extenstion
+            // write file with .md extenstion
+            let markDownFilename = filename.replace('html', 'md');``
+            fs.writeFileSync(outputDir + markDownFilename, markdown);
+        });
+    });
+});
