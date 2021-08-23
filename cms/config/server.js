0405409d0a9fd24ae70598efaa24fc9dd2057fe9
Joe Fearnley <joe.fearnley@gmail.com>
2021-08-23 08:06:06 -0400

    Removing cms and api apps.

--- cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e0b29323c63cece9e7757813db1bbd25'),
-    },
-  },
-});
--- cms/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'd6483437-12b4-4b1c-a3b8-a93354ac3e7a'
-};
