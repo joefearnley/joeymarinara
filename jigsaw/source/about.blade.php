451cd1c7c758813fca8bd7ab0a52b6685efd97c9
Joe Fearnley <joe.fearnley@gmail.com>
2021-09-01 11:57:48 -0400

    Update about page.

--- jigsaw/source/about.blade.php
+++ jigsaw/source/about.blade.php
@@ -11,9 +11,5 @@ description: A little bit about the site
         alt="About image"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+    <p class="mb-6">Welcome to Joey Marinara. My name is Joe and I really like pizza. This web site is my attempt to eat at/from local pizza joints around (but not limited to) Southwest Michigan. I am one of the pickiest eaters I know so don't expect much but a few laughs and some interesting pictures.</p>
 @endsection
