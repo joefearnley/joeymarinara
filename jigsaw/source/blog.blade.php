ac5d8cbcdfbccd284de24d60d11230eca03db777
Joe Fearnley <joe.fearnley@gmail.com>
2021-09-16 22:22:37 -0400

    Updating pagination to only to previous/next links

--- jigsaw/source/blog.blade.php
+++ jigsaw/source/blog.blade.php
@@ -3,7 +3,7 @@ title: Archives
 description: The list of posts for the site
 pagination:
     collection: posts
-    perPage: 4
+    perPage: 10
 ---
 @extends('_layouts.master')
 
@@ -18,29 +18,21 @@ pagination:
     @endforeach
 
     @if ($pagination->pages->count() > 1)
-        <nav class="flex text-base my-8">
+        <nav class="flex justify-between text-base my-8">
             @if ($previous = $pagination->previous)
                 <a
                     href="{{ $previous }}"
                     title="Previous Page"
-                    class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
-                >&LeftArrow;</a>
+                    class="rounded mr-3 px-5 py-3"
+                >&LeftArrow; newer</a>
             @endif
-
-            @foreach ($pagination->pages as $pageNumber => $path)
-                <a
-                    href="{{ $path }}"
-                    title="Go to Page {{ $pageNumber }}"
-                    class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3 {{ $pagination->currentPage == $pageNumber ? 'text-brown' : 'text-red' }}"
-                >{{ $pageNumber }}</a>
-            @endforeach
 
             @if ($next = $pagination->next)
                 <a
                     href="{{ $next }}"
                     title="Next Page"
-                    class="bg-gray-200 hover:bg-gray-400 rounded mr-3 px-5 py-3"
-                >&RightArrow;</a>
+                    class="rounded mr-3 px-5 py-3"
+                >older &RightArrow;</a>
             @endif
         </nav>
     @endif
